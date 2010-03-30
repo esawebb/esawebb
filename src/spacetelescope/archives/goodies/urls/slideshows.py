08928c9382b7cda60db2e01c7350e250f8971132
lcgomes <devnull@localhost>
2010-03-30 13:40:11 +0000

    spacetelescope archive definitions, admin, migration, resource migration, etc.

---
+++ src/spacetelescope/archives/goodies/urls/slideshows.py
@@ -0,0 +1,13 @@
+# Spacetelescope
+# Copyright 2007-2008 ESA/Hubble
+#
+# Authors:
+#   Lars Holm Nielsen <lnielsen@eso.org>
+#   Luis Clara Gomes <lcgomes@eso.org>
+#
+
+from djangoplicity.archives import urlpatterns_for_options
+from spacetelescope.archives.goodies.options import *
+
+
+urlpatterns = urlpatterns_for_options( SlideShowOptions )
