22e5287ed0690287ec665cda7fa6e3cf2e786ae5
lcgomes <devnull@localhost>
2010-04-15 10:16:48 +0000

    archive list search
    shadowbox enabled

--- static/js/site.js
+++ static/js/site.js
@@ -27,3 +27,31 @@
 $(document).ready(function () {
     $(this).extlink();
 });
+
+/*shadowbox */
+
+$(document).ready( function(){
+	var options = {
+		assetURL: MEDIA_URL + "djangoplicity/shadowbox3/",
+		handleLgImages: "resize",
+		players : ["img","html","flv","qt","wmp"],
+    };
+    Shadowbox.init(options);	
+
+
+}); 
+
+/* onFocus/onBlur events for search fields */
+function searchbox_focus( box, text ) {
+	if( box.value == text ){
+		box.value='';
+		box.className = box.className + '_ready';
+	}
+}
+
+function searchbox_blur( box, text ) {
+	if( box.value == '' ){
+		box.value=text;
+		box.className = box.className.split('_ready',1)[0];
+	}
+}
