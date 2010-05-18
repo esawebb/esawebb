c42103be381195575162ef30737b3bb46291d9d0
lnielsen <devnull@localhost>
2010-05-18 10:19:43 +0000

    - Placed feed icon on list views.

--- src/spacetelescope/archives/feeds.py
+++ src/spacetelescope/archives/feeds.py
@@ -12,23 +12,25 @@ from models import *
 from options import *
 
 class AnnouncementFeed ( DjangoplicityArchiveFeed ):
-    title = 'Hubble Space Telescope Announcements'
-    link = '/announcements/'
-    description = 'Spacetelescope.org Announcements'
+	title = 'Hubble Space Telescope Announcements'
+	link = '/announcements/'
+	description = 'Spacetelescope.org Announcements'
 
-    #title_template = feedsconf.get('ReleaseFeedSettings','title_template','feeds/release_title.html')
-    #description_template = feedsconf.get('ReleaseFeedSettings','description_template','feeds/release_description.html')
-    
-    class Meta():
-        model = Announcement
-        options = AnnouncementOptions
-        latest_fieldname = Announcement.Archive.Meta.release_date_fieldname
-        enclosure_resources = {    ''   : 'resource_screen' }
-        default_query = AnnouncementOptions.Queries.default
-        category_query = None
-        items_to_display = 10
-            
-        
-    def item_enclosure_mime_type( self, item ):
-        return 'image/jpeg'
-    
+	#title_template = feedsconf.get('ReleaseFeedSettings','title_template','feeds/release_title.html')
+	#description_template = feedsconf.get('ReleaseFeedSettings','description_template','feeds/release_description.html')
+	
+	class Meta():
+		model = Announcement
+		options = AnnouncementOptions
+		latest_fieldname = Announcement.Archive.Meta.release_date_fieldname
+		enclosure_resources = {	''   : 'resource_screen' }
+		default_query = AnnouncementOptions.Queries.default
+		category_query = None
+		items_to_display = 10
+		archive_query = "default"
+		feed_url = "http://feeds.feedburner.com/hubble_announcements/"
+			
+		
+	def item_enclosure_mime_type( self, item ):
+		return 'image/jpeg'
+	
