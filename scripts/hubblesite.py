f8b7512389d2e7613514f74160610a2bf1fd1810
dneumayer <devnull@localhost>
2011-03-22 11:27:24 +0100

    added get_release_date feature

--- scripts/hubblesite.py
+++ scripts/hubblesite.py
@@ -1,8 +1,8 @@
 #
+# -*- coding: utf-8 -*-
+#
 # eso.org
 # Copyright 2011 ESO
-#
-# -*- coding: utf-8 -*-
 # Authors:
 #   Lars Holm Nielsen <lnielsen@eso.org>
 #   Dirk Neumayer <dirk.neumayer@gmail.com>
@@ -13,6 +13,10 @@
 
 import re
 import urllib2 
+
+from datetime import datetime, tzinfo
+import pytz
+from pytz import timezone
 
 import socket
 timeout = 60
@@ -36,6 +40,71 @@ def remove_void(text):
     text = text.replace("\n","")
     text = ' '.join(text.split())
     return text
+
+def get_release_date(link):
+    '''
+    returns the release date in UTC found on the http://hubblesite.org/newsc...../image/ site
+    
+                       <h2 class="date">  <strong>  August 13, 1990</strong> 12:00 AM (EDT) </h2>
+    
+    1990-June-22 12:00 AM EDT
+    1990-June-22 04:00 AM UTC
+
+    '''
+    pat = re.compile(r'<h2 class="date">.*?<strong>([\S]+)[\s]+([\S]+),[\s]*?([\S]+)[\s]*?</strong>[\s]+([\S]+):([\S]+)[\s]+([\S]+)[\s]+\(?([A-Z]+)\)?[\s]+</h2>') 
+    
+    release_date = None
+    link = release_images(link) # link now ends with .../image/ and points to the release images
+    
+    # read the website
+    try:
+        site = urllib2.urlopen(link)
+        text = site.read()
+        # remove all line breaks and double whitespace
+        text = remove_void(text)
+              
+        #pat = re.compile(r'<h2 class="date">.*?<strong>([\S]+)[\s]+([\S]+),[\s]*?([\S]+)[\s]*?</strong>[\s]+([\S]+):([\S]+)[\s]+([\S]+)[\s]+([\S]+)[\s]+</h2>') 
+        date = None
+        try:
+            date = pat.findall(text)[0]
+            #print date
+            B = str(date[0])  # %B     Locale's full month name.  'December'#
+            d = str(date[1])  # %d     Day of the month as a decimal number [01,31].
+            Y = str(date[2])  # %Y     Year with century as a decimal number.
+            I = str(date[3])  # %I     Hour (12-hour clock) as a decimal number [01,12].
+            M = str(date[4])  # %M     Minute as a decimal number [00,59].
+            S = '00'          # %S     Second as a decimal number [00,61].
+            p = str(date[5])  # %p     Locale's equivalent of either AM or PM.
+            Z = str(date[6])  # (%Z)   Time zone name (unfortunately only UTC is working).
+            datestring = Y + '-' + B + '-' + d + ' ' + I + ':' + M  + ' ' + p
+            
+            fmt = '%Y-%B-%d %I:%M %p'
+            naive = datetime.strptime(datestring,fmt)
+            
+            # take care of timezones
+            utc = timezone('UTC')
+            eastern = timezone('US/Eastern')
+            
+            tz = utc    
+            # for daylight saving 
+            is_dts = False
+            
+            if Z == 'EDT' or Z == 'EST':
+                #print 'US/Eastern'
+                tz = eastern
+                if Z == 'EDT': is_dts = True
+          
+            aware = tz.localize(naive, is_dts)
+            release_date = tz.normalize(aware)
+            release_date = release_date.astimezone(utc)  
+        except:
+            print "no datetime information found"
+            pass
+    except:
+        print link, ' could not be read'
+        pass
+    
+    return release_date # in UTC
 
 def release_images(link):
     '''
@@ -93,6 +162,17 @@ def list_links(url_images):   # [^>]
 
 if __name__ == '__main__':
     #routines to test the helper functions
+    
+
+    tests = [r'http://hubblesite.org/newscenter/archive/releases/1990/06/', 
+             r'http://hubblesite.org/newscenter/archive/releases/1990/06/image/a/']
+
+    for t in tests:
+        print t
+        print get_release_date(t).strftime('%Y-%B-%d %I:%M %p %Z')   
+          
+    exit()
+    
     print "list_links:"
     release = r'''http://hubblesite.org/newscenter/archive/releases/2005/37'''
    # release = r'''http://hubblesite.org/newscenter/archive/releases/2008/16/image/ce/'''
