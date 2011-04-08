7af75b53b6ee87c3545c636a9edd91e05de95755
lnielsen_eso <devnull@localhost>
2011-04-08 17:29:55 +0200

    - Added embedded page on top of image archive.

--- src/spacetelescope/appregister.py
+++ src/spacetelescope/appregister.py
@@ -22,6 +22,7 @@ PAGE_KEYS = {
 			  "shop_right_column" : "djangoplicity.shop_right_column",
 			  "free_order_form" : "djangoplicity.free_order_form",
 			  "frontpage_rightcol" : "spacetelescope.frontpage_rightcol",			  
+			  "image_archive_top" : "spacetelescope.image_archive_top",
 			}
 
 register_page_key( 'spacetelescope', PAGE_KEYS["login"], _('Login page'), _('Displayed above the login form.') )
@@ -29,5 +30,6 @@ register_page_key( 'spacetelescope', PAGE_KEYS["logout"], _('Logout page'), _('D
 register_page_key( 'spacetelescope', PAGE_KEYS["hubblecast_sidebar"], _('Hubblecast Sidebar'), _('Right-column displayed on the hubblecast feature page') )
 register_page_key( 'spacetelescope', PAGE_KEYS["access_denied"], _('Access Denied'), _('Displayed when users are denied access to embargoed/staging resources.') )
 register_page_key( 'spacetelescope', PAGE_KEYS["frontpage_rightcol"], _('Frontpage right column'), _('Displayed above archive picture of the day.') )
+register_page_key( 'spacetelescope', PAGE_KEYS["image_archive_top"], _('Image archive top'), _('Displayed just below the title in image archive list views.') )
 register_page_key( 'djangoplicity', PAGE_KEYS["shop_right_column"], _('Shop Right Column'), _('Displayed on shop frontpage in right column.') )
 register_page_key( 'djangoplicity', PAGE_KEYS["free_order_form"], _('Free Order Form'), _('Displayed message on the Free Order form.') )
