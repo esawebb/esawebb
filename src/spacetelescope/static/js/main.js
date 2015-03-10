04673bd2a1a0042ed6e799d88d149e59dd519e08
Mathias André <mandre@eso.org>
2015-03-10 17:33:29 +0100

    Add PR carousel

--- src/spacetelescope/static/js/main.js
+++ src/spacetelescope/static/js/main.js
@@ -204,24 +204,23 @@ $(document).ready(function() {
 		if (current > liCount)
 			liCount = current;
 	});
-	console.log(liCount);
 	level1.css('height', liCount * 25 + 30);
 	$('.level0 > li').hover(function() {
 	});
 });
 
 
-// // Prepare PR carousel
-// $(document).ready(function(){
-	// $('#pr-carousel').slick({
-		// infinite: true,
-		// lazyLoad: 'progressive',
-		// prevArrow: '<button type="button" class="slide-prev"><span class="fa fa-angle-left"></span</button>',
-		// nextArrow: '<button type="button" class="slide-next"><span class="fa fa-angle-right"></span></button>',
+// Prepare PR carousel
+$(document).ready(function(){
+	$('#pr-carousel').slick({
+		infinite: true,
+		lazyLoad: 'progressive',
+		prevArrow: '<button type="button" class="slide-prev"><span class="fa fa-angle-left"></span</button>',
+		nextArrow: '<button type="button" class="slide-next"><span class="fa fa-angle-right"></span></button>',
 		// autoplay: true,
 		// autoplaySpeed: 10000
-	// });
-// });
+	});
+});
 
 // // Prepare Ann carousel
 // $(document).ready(function(){
