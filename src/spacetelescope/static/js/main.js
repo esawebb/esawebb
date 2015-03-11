3d89d7f61cd09208cf50e25255b78f2ce98123d1
Mathias André <mandre@eso.org>
2015-03-11 15:12:55 +0100

    Add hubblecats, potw on frontpage

--- src/spacetelescope/static/js/main.js
+++ src/spacetelescope/static/js/main.js
@@ -259,27 +259,28 @@ $(document).ready(function(){
 });
 
 
-// // Prepare POTW carousel
+// Prepare POTW carousel
+$(document).ready(function(){
+	$('#potw-carousel').slick({
+		infinite: false,
+		lazyLoad: 'ondemand',
+		prevArrow: '<button type="button" class="frontpage-slide-prev"><span class="fa fa-angle-left"></span</button>',
+		nextArrow: '<button type="button" class="frontpage-slide-next"><span class="fa fa-angle-right"></span></button>',
+		appendArrows: '#potw-carousel-wrapper .section-navigation'
+	});
+});
+
+
+// Prepare Hubblecast carousel
 // $(document).ready(function(){
-	// $('#potw-carousel').slick({
+	// $('#esocast-carousel').slick({
 		// infinite: false,
-		// lazyLoad: 'ondemand',
-		// prevArrow: '<button type="button" class="slide-prev"><span class="fa fa-angle-left"></span</button>',
-		// nextArrow: '<button type="button" class="slide-next"><span class="fa fa-angle-right"></span></button>'
+		// lazyLoad: 'progressive',
+		// prevArrow: '<button type="button" class="frontpage-slide-prev"><span class="fa fa-angle-left"></span</button>',
+		// nextArrow: '<button type="button" class="frontpage-slide-next"><span class="fa fa-angle-right"></span></button>',
+		// appendArrows: '#huublecast-carousel-wrapper .section-navigation'
 	// });
 // });
-
-
-// // Prepare ESOCast carousel
-// // $(document).ready(function(){
-	// // $('#esocast-carousel').slick({
-		// // infinite: false,
-		// // lazyLoad: 'progressive',
-		// // prevArrow: '<button type="button" class="slide-prev"><span class="fa
-		// // fa-angle-left"></span</button>',
-		// // nextArrow: '<button type="button" class="slide-next"><span class="fa fa-angle-right"></span></button>'
-	// // });
-// // });
 
 
 // // Prepare Highlights carousel
--- src/spacetelescope/views.py
+++ src/spacetelescope/views.py
@@ -7,13 +7,12 @@
 #   Lars Holm Nielsen <lnielsen@eso.org>
 #   Luis Clara Gomes <lcgomes@eso.org>
 #
-from datetime import datetime
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.cache import cache_page
 from djangoplicity.announcements.models import Announcement
-from djangoplicity.media.models import Image, Video, PictureOfTheWeek
-from djangoplicity.media.options import ImageOptions, VideoOptions
+from djangoplicity.media.models import Video, PictureOfTheWeek
+from djangoplicity.media.options import VideoOptions, PictureOfTheWeekOptions
 from djangoplicity.releases.models import Release
 
 
@@ -21,19 +20,13 @@ from djangoplicity.releases.models import Release
 def frontpage( request ):
 	""" Front page """
 
-	now = datetime.now()
-	potd = ImageOptions.Queries.default.queryset( Image, ImageOptions, request )[0][now.day]
-	votd = VideoOptions.Queries.default.queryset( Video, VideoOptions, request )[0][now.day]
 	hubblecasts = VideoOptions.Queries.category.queryset( Video, VideoOptions, request, stringparam='hubblecast' )[0].order_by('-release_date', )[:5]
 
 	return render_to_response('frontpage.html', {
 		'releases': Release.get_latest_release( 5 ),
-		'potw': PictureOfTheWeek.get_latest(),
+		'potws': PictureOfTheWeekOptions.Queries.default.queryset( PictureOfTheWeek, PictureOfTheWeekOptions, request )[0][:10],
 		'announcements': Announcement.get_latest_announcement(5, only_featured=True),
 		'hubblecasts': hubblecasts,
-		'potd': potd,
-		'votd': votd,
-		#'announcements': Announcement.get_latest_release( settings.FRONTPAGE_PRESSRELEASES_LEN ),
 	}, context_instance=RequestContext(request) )
 
 
