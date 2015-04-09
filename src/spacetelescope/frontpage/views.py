892b658c72890ac4a786932da85dd6206744bd21
Mathias André <mandre@eso.org>
2015-04-09 14:40:31 +0200

    Increase number of hubblecasts on frontpage

--- src/spacetelescope/frontpage/views.py
+++ src/spacetelescope/frontpage/views.py
@@ -25,7 +25,7 @@ class FrontpageView(TemplateView):
 
 		context['announcements'] = Announcement.get_latest_announcement(5, only_featured=True)
 		context['highlights'] = Highlight.objects.filter(published=True)
-		context['hubblecasts'] = VideoOptions.Queries.category.queryset(Video, VideoOptions, self.request, stringparam='hubblecast')[0].order_by('-release_date',)[:5]
+		context['hubblecasts'] = VideoOptions.Queries.category.queryset(Video, VideoOptions, self.request, stringparam='hubblecast')[0].order_by('-release_date',)[:10]
 		context['potws'] = PictureOfTheWeekOptions.Queries.default.queryset(PictureOfTheWeek, PictureOfTheWeekOptions, self.request)[0][:10]
 		context['releases'] = Release.get_latest_release(5)
 		context['top100'] = ImageOptions.Queries.top100.queryset(Image, ImageOptions, self.request)[0][:20]
