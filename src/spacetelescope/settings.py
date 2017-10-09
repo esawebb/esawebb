8347079511c1bdcd8095ebae2c602dde8f538a9f
Mathias André <mandre@eso.org>
2017-10-09 11:50:32 +0200

    Fix celerybeat schedule

--- src/spacetelescope/settings.py
+++ src/spacetelescope/settings.py
@@ -740,6 +740,11 @@ CELERYBEAT_SCHEDULE = {
 		'task': 'djangoplicity.contentserver.tasks.check_content_server_resources',
 		'schedule': crontab(minute=0, hour=4),
 	},
+	'check-content-server-resources-all': {
+		'task': 'djangoplicity.contentserver.tasks.check_content_server_resources',
+		'schedule': crontab(minute=0, hour=10, day_of_week='sun'),
+		'kwargs': {'last': 15000},  # Large enough to check all resources
+	},
 	'cdn77-purge-prefetch': {
 		'task': 'djangoplicity.contentserver.cdn77_tasks.purge_prefetch',
 		'schedule': crontab(minute='*/10'),  # Every 10 minutes
