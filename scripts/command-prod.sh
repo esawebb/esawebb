8d3117324bba7941724252b5f463822f4176404c
Edison Arango <edisonarab@gmail.com>
2020-12-02 19:57:28 -0500

    avoid automatic migration for now

--- scripts/command-prod.sh
+++ scripts/command-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python manage.py migrate
+# python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn --workers 4 --log-level info --timeout 300 --bind 0.0.0.0:8000 spacetelescope.wsgi:application
