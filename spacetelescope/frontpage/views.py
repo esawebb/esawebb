3c7eeee04de6424ffa3bacf87d6b6feefc97ca08
dpalacios <diferpaho@gmail.com>
2021-12-14 09:41:47 -0500

    add soon.html in frontpage.html

--- spacetelescope/frontpage/views.py
+++ spacetelescope/frontpage/views.py
@@ -23,7 +23,7 @@ from spacetelescope.frontpage.models import Highlight
 
 class FrontpageView(TemplateView):
 
-    template_name = 'soon.html'
+    template_name = 'frontpage.html'
 
     def get_context_data(self, **kwargs):
         context = super(FrontpageView, self).get_context_data(**kwargs)
