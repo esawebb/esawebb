5f5f7d139e8c57f95e6ebd79d29332d45bce8ae2
dpalacios <diferpaho@gmail.com>
2022-04-27 22:08:06 -0500

    changes in the potw  url

--- spacetelescope/urls.py
+++ spacetelescope/urls.py
@@ -80,8 +80,8 @@ urlpatterns += [
     url( r'^alive-check.dat$', djangoplicity.views.alive_check ),
 
     # Media Archive
-    url( r'^images/potw/', include('djangoplicity.media.urls_potw'), { 'model': PictureOfTheWeek, 'options': PictureOfTheWeekOptions } ),
-    url( r'^images/comparisons/', include('djangoplicity.media.urls_imagecomparisons'), { 'model': ImageComparison, 'options': ImageComparisonOptions } ),
+    url( r'^n3w-fr0nt/images/potw/', include('djangoplicity.media.urls_potw'), { 'model': PictureOfTheWeek, 'options': PictureOfTheWeekOptions } ),
+    url( r'^n3w-fr0nt/images/comparisons/', include('djangoplicity.media.urls_imagecomparisons'), { 'model': ImageComparison, 'options': ImageComparisonOptions } ),
     url( r'^n3w-fr0nt/images/', include('djangoplicity.media.urls_images'), { 'model': Image, 'options': ImageOptions } ),
     #( r'^news/feed/(?P<url>.*)/?$', 'django.contrib.syndication.views.feed', { 'feed_dict': ReleaseOptions.get_feeds() } ),
     url( r'^n3w-fr0nt/news/', include('djangoplicity.releases.urls'), { 'model': Release, 'options': ReleaseOptions } ),
