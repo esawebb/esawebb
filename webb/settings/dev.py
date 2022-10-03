9b0240a083a5ed99767793d2ece11f070166bb99
Alejandro Vivas <alejo.vivas.av@gmail.com>
2022-10-03 08:54:45 -0500

    Content server configuration was separated, prod environment from staging

--- webb/settings/dev.py
+++ webb/settings/dev.py
@@ -102,3 +102,78 @@ EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 # This keys are provided by google for testing purposes
 RECAPTCHA_PUBLIC_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
 RECAPTCHA_PRIVATE_KEY = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+
+##################
+# CONTENT SERVER #
+##################
+MEDIA_CONTENT_SERVERS = {
+    'CDN77': CDN77ContentServer(
+        name='CDN77',
+        formats={
+            'djangoplicity.media.models.images.Image': (
+                'large',
+                'publicationjpg',
+                'screen',
+                'wallpaper1',
+                'wallpaper2',
+                'wallpaper3',
+                'wallpaper4',
+                'wallpaper5',
+                'thumb150y',
+                'thumb300y',
+                'thumb350x',
+                'thumb700x',
+                'newsfeature',
+                'news',
+                'banner1920',
+                'screen640',
+                'zoomable',
+            ),
+            'djangoplicity.media.models.videos.Video': (
+                'videoframe',
+                'small_flash',
+                'medium_podcast',
+                'medium_mpeg1',
+                'medium_flash',
+                'large_qt',
+                'broadcast_sd',
+                'hd_and_apple',
+                'hd_broadcast_720p50',
+                'hd_1080p25_screen',
+                'hd_1080p25_broadcast',
+                'ultra_hd',
+                'ultra_hd_h265',
+                'ultra_hd_broadcast',
+                'dome_8kmaster',
+                'dome_4kmaster',
+                'dome_2kmaster',
+                'dome_mov',
+                'dome_preview',
+                'cylindrical_4kmaster',
+                'cylindrical_8kmaster',
+                'cylindrical_16kmaster',
+                'news',
+            ),
+        },
+        url='https://cdn3.esawebb.org/',
+        url_bigfiles='https://cdn4.esawebb.org/',
+        remote_dir='/www/',
+        host='push-12.cdn77.com',
+        username=get_secret('CDN_STORAGE_USERNAME'),
+        password=get_secret('CDN_STORAGE_PASSWORD'),
+        api_login=get_secret('CDN_API_LOGIN'),
+        api_password=get_secret('CDN_API_PASSWORD'),
+        apiv3_token=get_secret('CDN_API_TOKEN'),
+        cdn_id='1047011652',
+        cdnv3_id='1047011652',
+        cdn_id_bigfiles='1653546147',
+        cdnv3_id_bigfiles='1653546147',
+    ),
+}
+
+MEDIA_CONTENT_SERVERS_CHOICES = (
+    ('', 'Default'),
+    ('CDN77', 'CDN77'),
+)
+
+DEFAULT_MEDIA_CONTENT_SERVER = 'CDN77'
