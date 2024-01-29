55b1610e3599342d09e249778203f80db1053d89
cparrado <parradocristian07@gmail.com>
2024-01-29 12:07:55 -0500

    Remove 20 unnecessary spaces in 'static/q/spacemailform.php' file'

--- static/q/spacemailform.php
+++ static/q/spacemailform.php
@@ -6,10 +6,10 @@ $subject 	= trim($_POST['SPACEsubject']);
 $fields 	= $_POST['SPACEfields'];
 
 // Check mailto
-$mailval = explode('@', $mailto );
+$mailval = explode('@', $mailto);
 
-if( !(count($mailval) == 2 && ($mailval[1] == 'hankat.dk') or ($mailval[1] == 'eso.org') or ($mailval[1] == 'webb.org')) ) {
-	reportError("Invalid mail-to address");
+if( !(count($mailval) == 2 && ($mailval[1] == 'hankat.dk') or ($mailval[1] == 'eso.org') or ($mailval[1] == 'webb.org'))){
+  reportError("Invalid mail-to address");
 }
 
 if (get_magic_quotes_gpc()) {
@@ -20,27 +20,27 @@ if (get_magic_quotes_gpc()) {
 //
 // DO NOT USE $fields IN DYNMAIC STRINGS!!!!!!!!!!! 
 
-if( !preg_match_all('/ID_([a-z_]+)/', $fields, $fieldsArray ) ) {
+if(!preg_match_all('/ID_([a-z_]+)/', $fields, $fieldsArray)){
 	reportError('No fields defined');
 }
 
-for( $i = 0; $i < count($fieldsArray[0]); $i++ ) {
-	if( !isset( $_POST[$fieldsArray[1][$i]] ) ) {
+for($i = 0; $i < count($fieldsArray[0]); $i++) {
+	if(!isset($_POST[$fieldsArray[1][$i]])) {
 		$_POST[$fieldsArray[1][$i]] = '';
 	}
-	$fields = str_replace( $fieldsArray[0][$i], $_POST[$fieldsArray[1][$i]], $fields );
+	$fields = str_replace( $fieldsArray[0][$i], $_POST[$fieldsArray[1][$i]], $fields);
 }
 
-if( !mail( $mailto, $subject, $fields ) ) {
+if(!mail($mailto, $subject, $fields)) {
 	reportError("Mail couldn't be sent");
 }
 
-header('Location: ' . $redirect );
+header('Location: ' . $redirect);
 exit;
 
-function reportError($x) {
-	if( is_string($x) ) {
-		trigger_error( 'Space MAILFORM - ' . $x, E_USER_ERROR );
+function reportError($x){
+	if(is_string($x)){
+		trigger_error( 'Space MAILFORM - ' . $x, E_USER_ERROR);
 	}
 }
 ?>
